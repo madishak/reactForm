cc1e527e9d2ef5801468bf1c0d151685e4172e51
Madina Karachai <medinakarachai@mail.ru>
2022-03-17 18:29:08 +0300

    Stated to do contacts container

--- src/containers/Contacts/index.tsx
+++ src/containers/Contacts/index.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { TextField, Button, Typography } from "@mui/material";
 
 const Contacts = () => {
-    return (
-        <div>Contacts</div>
-    )
+
+
+    const handleChange = () => {
+
+    }
+
+
+    const handleAdd = () => {
+
+    }
+
+  return (
+    <div>
+      Contacts
+      <form>
+        <Typography>Error</Typography>
+        <TextField
+      
+          label="username"
+          variant="standard"
+          required={true}
+          name="username"
+        
+        />
+        <Button type="submit" variant="contained">
+          ADD
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default Contacts;
